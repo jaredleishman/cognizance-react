3494d28771f2edc4f4a24afe34cc11e02229f80a
Jared Leishman <jleishman@brevium.com>
2019-04-15 19:01:58 -0600

    scroll bar isn't loading other photos

--- src/components/NewEntry.js
+++ src/components/NewEntry.js
@@ -94,6 +94,7 @@ class NewEntry extends Component {
 				{ nextPageToken: nextPageToken },
 				{ headers: {'Content-Type': 'text/plain'} })
 				.then((response) => {
+					console.log(response)
 					this.setState({ mediaItems: [...this.state.mediaItems, ...response.data.mediaItems], nextPageToken: response.data.nextPageToken })					
 				})
 		}
